09c3bd3a87a5b86dcc48955f7bf54955bf2d87f1
zhco00 <mkni0216@gmail.com>
2024-11-03 13:59:06 +0900

    루트 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '@/pages/home/home';
 import Apply from '@/pages/apply';
 import './App.css';
 
 function App() {
   return (
-    <>
-      <Router basename={process.env.PUBLIC_URL}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/apply" element={<Apply />} />
-        </Routes>
-      </Router>
-    </>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/apply" element={<Apply />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,9 @@ import { Provider } from 'react-redux';
 
 // ** Router Imports
 import App from './App.jsx';
-import store from './store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
+  <Provider>
     <App />
   </Provider>,
 );
